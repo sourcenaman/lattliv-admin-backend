5b3fe902d764605e2c401e40b81d9e4142b812c2
Naman Vashishth <namanvashishth@Namans-MacBook-Air.local>
2022-05-21 13:48:56 +0530

    jwt env secret added

--- api/helpers/generate-new-jwt-token.js
+++ api/helpers/generate-new-jwt-token.js
@@ -23,7 +23,7 @@ module.exports = {
       sub: inputs.subject, // subject
       iss: "LogRocket Sails API", // issuer
     };
-    const secret = sails.config.jwtSecret;
+    const secret = sails.config.jwtSecret || process.env.JWT_SECRET;
     const token = jwt.sign(payload, secret, { expiresIn: "1d" });
     return token;
   },
