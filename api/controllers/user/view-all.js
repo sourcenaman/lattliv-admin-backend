e72256651d8c651ce0debd011d8de1eb66d899ac
Naman Vashishth <namanvashishth@Namans-MacBook-Air.local>
2022-06-06 10:47:31 +0530

    state data added in user view all

--- api/controllers/user/view-all.js
+++ api/controllers/user/view-all.js
@@ -14,7 +14,9 @@ module.exports = {
 
 
   fn: async function (inputs, exits) {
-    var users = await User.find( { state: { "!=": 3 }} ).sort("id DESC");
+    var users = await User.find({ state: { "!=": 3 } })
+      .populate("state")
+      .sort("id DESC");
     return users ? exits.success(users) : exits.notFound({ error: `User with ID ${inputs.id} not found.` });
   }
 };
