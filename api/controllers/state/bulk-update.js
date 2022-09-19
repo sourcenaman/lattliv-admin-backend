b71a720764d16fa5da32219dd717ff825051f768
Naman Vashishth <namanvashishth@Namans-MacBook-Air.local>
2022-09-19 21:36:00 +0530

    bulk update deletedBy approvedBy added

--- api/controllers/state/bulk-update.js
+++ api/controllers/state/bulk-update.js
@@ -4,37 +4,52 @@ module.exports = {
   description: "Update product.",
 
   inputs: {
-      ids: {
-          type: ["number"],
-          required: true
-      },
-      state: {
-          type: "number",
-          required: true
-      },
-      type: {
-          type: "string",
-          required: true
-      }
+    ids: {
+      type: ["number"],
+      required: true,
+    },
+    state: {
+      type: "number",
+      required: true,
+    },
+    type: {
+      type: "string",
+      required: true,
+    },
   },
 
   exits: {},
 
   fn: async function (inputs) {
     // All done.
-    console.log(inputs);
+    let user = this.req.session.user;
+    inputs["deletedBy"] = inputs.state == 3 ? user.id : "";
+    inputs["approvedBy"] = inputs.state == 2 ? user.id : "";
     let resp = null;
-    switch(inputs.type){
+    switch (inputs.type) {
       case "product":
-        resp = await Product.update({ id: inputs.ids }, { state: inputs.state }).fetch();
+        resp = await Product.update(
+          { id: inputs.ids },
+          {
+            state: inputs.state,
+            deletedBy: inputs.deletedBy,
+            approvedBy: inputs.approvedBy,
+          }
+        ).fetch();
         break;
       case "category":
-        resp = await Category.update({ id: inputs.ids }, { state: inputs.state }).fetch();
+        resp = await Category.update(
+          { id: inputs.ids },
+          {
+            state: inputs.state,
+            deletedBy: inputs.deletedBy,
+            approvedBy: inputs.approvedBy,
+          }
+        ).fetch();
         break;
       default:
-        resp = "Invalid type."
+        resp = "Invalid type.";
     }
-    console.log(resp)
     return resp;
   },
 };
